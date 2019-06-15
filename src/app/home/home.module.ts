c4ac13447c1c0fdcc8121b5fca1ede0d67378240
Sean Halls <sean@prismwp.com>
2019-06-14 19:48:42 -0700

    Updates to the shell and home module. Scrapping starter layout and getting to a blank canvas essentially.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,12 +7,11 @@ import { CoreModule } from '@app/core';
 import { SharedModule } from '@app/shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { QuoteService } from './quote.service';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, CoreModule, SharedModule, IonicModule, HomeRoutingModule],
   entryComponents: [HomeComponent],
   declarations: [HomeComponent],
-  providers: [QuoteService]
+  providers: []
 })
 export class HomeModule {}
