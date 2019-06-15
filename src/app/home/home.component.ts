80b35bed9d5516958c40be43b4864f379930b091
Sean Halls <sean@prismwp.com>
2019-06-15 14:42:53 -0700

    Updting shell stuff, adding a logging component for us. Testing that out.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
-
-import { QuoteService } from './quote.service';
+import { Logger, LoggingService } from 'ionic-logging-service';
 
 @Component({
   selector: 'app-home',
@@ -9,22 +8,32 @@ import { QuoteService } from './quote.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  quote: string | undefined;
   isLoading = false;
-
-  constructor(private quoteService: QuoteService) {}
+  logger: Logger;
+  constructor(loggingService: LoggingService) {
+    this.logger = loggingService.getLogger('MyApp.MyComponent');
+    const methodName = 'ctor';
+    this.logger.entry(methodName);
+    this.logger.exit(methodName);
+    return;
+  }
 
   ngOnInit() {
     this.isLoading = true;
-    this.quoteService
-      .getRandomQuote({ category: 'dev' })
-      .pipe(
-        finalize(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe((quote: string) => {
-        this.quote = quote;
-      });
+    this.getUserMenu(1);
   }
+  /**
+   * A function to get the appropriate user menu to display on the initial dashboard.
+   * Question: Do we include the option to skip this screen and make a screen a favorite?
+   */
+  public getUserMenu = function(userId: number) {
+    const methodName = arguments.callee.toString();
+    debugger;
+    this.logger.entry(methodName, userId);
+    try {
+    } catch (e) {
+      this.logger.error(methodName, 'some error', e);
+    }
+    console.log('Called function get user menu.');
+  };
 }
