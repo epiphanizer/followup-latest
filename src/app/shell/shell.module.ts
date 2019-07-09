57af735ca4c83ad893eca100da8bf2a1306ea67b
Sean Halls <sean@prismwp.com>
2019-07-08 22:09:01 -0700

    Work on toolbar profile nav component functionality.'

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -4,11 +4,20 @@ import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { ToolbarLogoModule } from './toolbar-logo.module';
+import { ToolbarProfileNavModule } from './toolbar-profile-nav.module';
 import { UserCorkBoardModule } from './user-cork-board.module';
 import { ShellComponent } from './shell.component';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, IonicModule, RouterModule, ToolbarLogoModule, UserCorkBoardModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    IonicModule,
+    RouterModule,
+    ToolbarLogoModule,
+    ToolbarProfileNavModule,
+    UserCorkBoardModule
+  ],
   entryComponents: [ShellComponent],
   declarations: [ShellComponent]
 })
