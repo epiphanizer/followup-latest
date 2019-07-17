899a9c086687dcb6ef8a450c00cd4548291daddb
Sean Halls <seanhalls@gmail.com>
2019-07-17 03:34:37 -0600

    Scaffolding and throwing in a basic patient service

--- src/app/modules/call-queue/call-queue-patient-listing/call-queue-patient-listing.component.ts
+++ src/app/modules/call-queue/call-queue-patient-listing/call-queue-patient-listing.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientService } from '@app/modules/patient/patient.service.ts';
+import { Observable } from 'rxjs';
 
 @Component({
+  providers: [PatientService],
   selector: 'app-call-queue-patient-listing',
   templateUrl: './call-queue-patient-listing.component.html',
   styleUrls: ['./call-queue-patient-listing.component.scss']
 })
 export class CallQueuePatientListingComponent implements OnInit {
-  constructor() {}
-
+  constructor(patientService: PatientService) {}
+  private $patients: Observable<any> | null = null;
   ngOnInit() {}
 
   public sortPatientsByCallDate = function() {};
   public sortPatientsByDischargeDate = function() {};
+  public toggleAscDesc = function() {};
   public toggleOperationSidebarMenu = function() {};
 }
