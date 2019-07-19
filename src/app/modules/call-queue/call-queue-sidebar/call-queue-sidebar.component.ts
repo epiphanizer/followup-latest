c5c1bad7838a6d1cbedd1ffeb7c305fffa8f2587
Sean Halls <seanhalls@gmail.com>
2019-07-19 03:02:53 -0600

    work on animating sidebar

--- src/app/modules/call-queue/call-queue-sidebar/call-queue-sidebar.component.ts
+++ src/app/modules/call-queue/call-queue-sidebar/call-queue-sidebar.component.ts
@@ -31,6 +31,20 @@ import {
           opacity: 0
         })
       ),
+      trigger('turnArrow', [
+        state(
+          'open',
+          style({
+            rotate: '0deg'
+          })
+        ),
+        state(
+          'closed',
+          style({
+            rotate: '-90deg'
+          })
+        )
+      ]),
       transition('open => closed', [animate('0.5s')]),
       transition('closed => open', [animate('0.25s')])
     ])
