03af2bb9262611dfc0d62d5a17ed76ea4297a8c6
Sean Halls <sean@prismwp.com>
2019-07-26 21:59:15 -0700

    scaffolding global dialog in shared folder

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { LoaderComponent } from './loader/loader.component';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
   imports: [IonicModule, CommonModule],
-  declarations: [LoaderComponent],
+  declarations: [LoaderComponent, DialogComponent],
   exports: [LoaderComponent]
 })
 export class SharedModule {}
