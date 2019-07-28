87d6ff96d761010c6035d1cfc0dfbc95c7795fe0
Sean Halls <sean@prismwp.com>
2019-07-28 13:24:21 -0700

    Importing patient call module, builds

--- src/app/modules/patient/patient.module.ts
+++ src/app/modules/patient/patient.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientRoutingModule } from './patient-routing.module';
+import { PatientCallModule } from './patient-call/patient-call.module';
+import { PatientNotesModule } from './patient-notes/patient-notes.module';
+import { PatientAvatarModule } from './patient-avatar/patient-avatar.module';
+
 import { PatientComponent } from './patient.component';
 import { PatientAvatarComponent } from '@app/modules/patient/patient-avatar/patient-avatar.component';
 import { PatientAddComponent } from '@app/modules/patient/patient-add/patient-add.component';
@@ -8,6 +12,6 @@ import { PatientEditComponent } from '@app/modules/patient/patient-edit/patient-
 
 @NgModule({
   declarations: [PatientComponent, PatientAvatarComponent, PatientAddComponent, PatientEditComponent],
-  imports: [CommonModule, PatientRoutingModule]
+  imports: [CommonModule, PatientRoutingModule, PatientCallModule, PatientNotesModule, PatientAvatarModule]
 })
 export class PatientModule {}
