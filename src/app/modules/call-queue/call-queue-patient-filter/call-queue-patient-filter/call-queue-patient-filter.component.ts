ce6e9b382c595055c789562aaee63ca60c26d16c
Sean Halls <sean@prismwp.com>
2019-08-01 16:29:00 -0700

    Work on patient call queue filter

--- src/app/modules/call-queue/call-queue-patient-filter/call-queue-patient-filter/call-queue-patient-filter.component.ts
+++ src/app/modules/call-queue/call-queue-patient-filter/call-queue-patient-filter/call-queue-patient-filter.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { User, UserService } from '@app/modules/user/user.service';
-
 import { AuthenticationService } from '@app/core';
+import { Patient, PatientService } from '@app/modules/patient/patient.service';
 
 @Component({
   providers: [AuthenticationService, UserService],
@@ -11,11 +12,25 @@ import { AuthenticationService } from '@app/core';
   styleUrls: ['./call-queue-patient-filter.component.scss']
 })
 export class CallQueuePatientFilterComponent implements OnInit {
+  @Input() operationId: number;
   user: User;
-  constructor(private authService: AuthenticationService) {}
+  patients: Array<Patient> = [];
+  patients$: Observable<Patient[]>;
+  constructor(private authService: AuthenticationService, private patientService: PatientService) {}
   ngOnInit() {
     this.authService.getUser().then((result: any) => {
       this.user = this.authService.user;
     });
+    this.patients$ = this.patientService.getPatientListByOperationId(this.operationId).pipe(
+      map((patients: Patient[]) => {
+        map((patient: Patient) => {
+          this.patients.push(patient);
+        });
+        return patients;
+      })
+    );
+  }
+  public searchPatientCallHistory() {
+    console.log('Searching patient call history');
   }
 }
