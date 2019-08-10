4181a908ede8d4846e5b7002396e08a1675bdbe6
Sean Halls <sean@prismwp.com>
2019-08-09 19:53:42 -0700

    Work on patient call service / stop button

--- src/app/modules/patient/patient-call/patient-call.service.ts
+++ src/app/modules/patient/patient-call/patient-call.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '@app/core';
 import { catchError, retry } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 export interface PatientCall {
   patientCallId: number;
@@ -59,4 +61,32 @@ export class PatientCallService {
       catchError(e => this.handleAsyncError(e)) // then handle the error
     );
   };
+
+  /**
+   * This hook is a bit misleading, it's actually the termination call in the frontend,
+   * but for our purposes, we are really just "putting" to edit the call termination time.
+   */
+  public endPatientCall(patientCallId: number) {
+    return this.http.put('patients/calls/' + patientCallId, {}).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(e => this.handleAsyncError(e)) // then handle the error
+    );
+  }
+
+  private handleAsyncError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      '<div class="alert alert-danger" role="alert"> \
+        <strong>Error</strong>: We had trouble connecting to the patient service\
+      </div>'
+    );
+  }
 }
