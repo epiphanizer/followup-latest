558eb86c177d105faf81e89d7205edaacd4d2dd1
Sean Halls <sean@prismwp.com>
2019-08-11 14:50:23 -0700

    Corrections all down the line on patient call notes

--- src/app/modules/patient/patient-detail/patient-call/patient-call-notes/patient-call-notes.component.ts
+++ src/app/modules/patient/patient-detail/patient-call/patient-call-notes/patient-call-notes.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PatientCall } from '../../patient-call/patient-call.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { PatientCallNotesService } from '@app/modules/patient/patient-call/patient-call-notes/patient-call-notes.service';
+import { PatientCallNotesService } from '@app/modules/patient/patient-detail/patient-call/patient-call-notes/patient-call-notes.service';
+import { PatientCallNotes } from './patient-call-notes.service';
+import { Observable } from 'rxjs';
 
 @Component({
+  providers: [PatientCallNotesService],
   selector: 'app-patient-call-notes',
   templateUrl: './patient-call-notes.component.html',
   styleUrls: ['./patient-call-notes.component.scss']
@@ -13,7 +16,7 @@ export class PatientCallNotesComponent implements OnInit {
   patientCallNotesForm: FormGroup;
   patientCallNotes: PatientCallNotes | null = null;
   patientCallNotes$: Observable<PatientCallNotes>;
-  constructor(fb: FormBuilder) {}
+  constructor(fb: FormBuilder, patientCallNotesService: PatientCallNotesService) {}
 
   ngOnInit() {}
   private createForm() {
---
+++ src/app/modules/patient/patient-detail/patient-call/patient-call-notes/patient-call-notes.service.ts
@@ -0,0 +1,46 @@
+import { throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpService } from '@app/core';
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+
+export interface PatientCallNotes {
+  patientCallNotes: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientCallNotesService {
+  constructor(private http: HttpService) {}
+  addPatientCallNotesByPatientCallId = function(patientCallId: number) {
+    return this.http.post('patients/calls/' + patientCallId).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(e => this.handleAsyncError(e)) // then handle the error
+    );
+  };
+
+  getPatientCallNotesByPatientCallId = function(patientCallId: number) {
+    return this.http.post('patients/calls/' + patientCallId + '/notes').pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(e => this.handleAsyncError(e)) // then handle the error
+    );
+  };
+
+  private handleAsyncError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      '<div class="alert alert-danger" role="alert"> \
+            <strong>Error</strong>: We had trouble connecting to the patient service\
+        </div>'
+    );
+  }
+}
