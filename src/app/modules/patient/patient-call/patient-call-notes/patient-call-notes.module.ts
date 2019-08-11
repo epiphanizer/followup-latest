da89ddefdf3ac4935777543bb313a070def46df2
Sean Halls <sean@prismwp.com>
2019-08-11 13:46:39 -0700

    scaffolding out call note highlighting service

--- src/app/modules/patient/patient-call/patient-call-notes/patient-call-notes.module.ts
+++ src/app/modules/patient/patient-call/patient-call-notes/patient-call-notes.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {
+  PatientCallNoteHighlight,
+  PatientCallNoteHighlightService
+} from '@app/modules/patient/patient-call/patient-call-notes/patient-call-notes.module';
 @NgModule({
   declarations: [],
   imports: [CommonModule]
 })
-export class PatientCallNotesModule {}
+export class PatientCallNotesModule {
+  patientCallNoteHighlights: [PatientCallNoteHighlights];
+}
