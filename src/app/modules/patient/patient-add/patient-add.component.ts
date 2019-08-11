491cdfb2b15b3367450e941e49fe0229615f3dce
Sean Halls <sean@prismwp.com>
2019-08-11 12:23:07 -0700

    Patient add form

--- src/app/modules/patient/patient-add/patient-add.component.ts
+++ src/app/modules/patient/patient-add/patient-add.component.ts
@@ -14,16 +14,77 @@ export class PatientAddComponent implements OnInit {
   patient: Patient;
   patient$: Observable<Patient>;
 
-  constructor(private formBuilder: FormBuilder, private patientService: PatientService) {}
+  constructor(private fb: FormBuilder, private patientService: PatientService) {}
 
   ngOnInit() {
     this.patient$ = this.patientService.addNewPatient();
     this.createForm();
   }
-  updateUserProfile() {
-    alert('updating user profile');
+  addPatientFormSubmit() {
+    alert('adding new patient');
   }
   private createForm() {
-    this.addPatientForm = this.formBuilder.group({});
+    this.addPatientForm = this.fb.group({
+      operation: this.fb.control({}),
+      patient: this.fb.group({
+        name: this.fb.control({}),
+        age: this.fb.control({}),
+        patientMedicalRecordNumber: this.fb.control({}),
+        patientContacts: this.fb.group({
+          primaryPatientContact: this.fb.group({
+            patientContactName: this.fb.control({}),
+            patientContactRelationship: this.fb.control({}),
+            patientContactCountryCodeNumber: this.fb.control({}),
+            patientContactAreaCodeNumber: this.fb.control({}),
+            patientContactPhoneNumber: this.fb.control({})
+          }),
+          alternatePatientContact1: this.fb.group({
+            patientContactName: this.fb.control({}),
+            patientContactRelationship: this.fb.control({}),
+            patientContactCountryCodeNumber: this.fb.control({}),
+            patientContactAreaCodeNumber: this.fb.control({}),
+            patientContactPhoneNumber: this.fb.control({})
+          }),
+          alternatePatientContact2: this.fb.group({
+            patientContactName: this.fb.control({}),
+            patientContactRelationship: this.fb.control({}),
+            patientContactCountryCodeNumber: this.fb.control({}),
+            patientContactAreaCodeNumber: this.fb.control({}),
+            patientContactPhoneNumber: this.fb.control({})
+          })
+        })
+      }),
+      physician: this.fb.group({
+        physicianName: this.fb.control({}),
+        physicianPhoneNumber: this.fb.control({})
+      }),
+      insurance: this.fb.group({
+        primaryInsurance: this.fb.control({}),
+        secondaryInsurance: this.fb.control({})
+      }),
+      dischargeInfo: this.fb.group({
+        patientAdmitDate: this.fb.control({}),
+        patientDischargeDate: this.fb.control({}),
+        patientStayLength: this.fb.control({}),
+        patientDischargedAma: this.fb.control({}),
+        patientDischargedTo: this.fb.control({})
+      }),
+      patientMedicalConditions: this.fb.group({
+        cardiacBoolean: this.fb.control({}),
+        sepsisBoolean: this.fb.control({}),
+        pulminaryBoolean: this.fb.control({}),
+        primaryDiagnosis: this.fb.control({}),
+        dischargedCondition: this.fb.control({})
+      }),
+      patientQuestionAnswers: this.fb.group({
+        painScaleAtIntake: this.fb.control({}),
+        mentalScaleAtIntake: this.fb.control({}),
+        patientMedicationBoolean: this.fb.control({}),
+        patientAppointmentScheduledWithDoctorBoolean: this.fb.control({}),
+        patientHomeHealthContactedBoolean: this.fb.control({}),
+        patientUrgencyRating: this.fb.control({}),
+        patientOtherDetails: this.fb.control({})
+      })
+    });
   }
 }
