b5a8daf5564ff2522c0cba938b43670e085d5910
Sean Halls <sean@prismwp.com>
2019-08-11 12:46:50 -0700

    Patient manager sidebar port

--- src/app/modules/patient/patient-manager-sidebar/patient-manager-sidebar.component.ts
+++ src/app/modules/patient/patient-manager-sidebar/patient-manager-sidebar.component.ts
@@ -1,12 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable, throwError, Subscription } from 'rxjs';
+import { catchError, retry, map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Operation } from '@app/modules/operation/operation.service';
+import { formatDate } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+  // ...
+} from '@angular/animations';
+
+import { User } from '@app/modules/user/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-patient-manager-sidebar',
   templateUrl: './patient-manager-sidebar.component.html',
-  styleUrls: ['./patient-manager-sidebar.component.scss']
+  styleUrls: ['./patient-manager-sidebar.component.scss'],
+  animations: [
+    trigger('expandSidebar', [
+      state(
+        'open',
+        style({
+          opacity: 1
+        })
+      ),
+      state(
+        'closed',
+        style({
+          opacity: 0
+        })
+      ),
+      transition('open => closed', [animate('0.5s')]),
+      transition('closed => open', [animate('0.25s')])
+    ]),
+    trigger('turnArrow', [
+      state(
+        'open',
+        style({
+          transform: 'rotate(0deg)'
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'rotate(-90deg)'
+        })
+      ),
+      transition('open => closed', [animate('0.125s')]),
+      transition('closed => open', [animate('0.125s')])
+    ])
+  ]
 })
 export class PatientManagerSidebarComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+  @Output() operationChangeEvent = new EventEmitter<number>();
+  selected: {
+    operation: Operation | null;
+  } = {
+    operation: null
+  };
+  isOpen = true;
+  constructor(private route: ActivatedRoute) {}
+  operations: Operation[];
+  user: User;
+  todaysDateDay: number;
+  ngOnInit() {
+    this.user = this.route.snapshot.data.user;
+    this.operations = this.user.operations;
+    this.selected.operation = this.user.operations[0];
+    this.todaysDateDay = parseInt(formatDate(new Date(), 'dd', 'en'));
+  }
+  setActiveOperation = function(operation: Operation) {
+    this.selected.operation = operation;
+    this.operationChangeEvent.emit(operation);
+  };
+  public toggleOperationSidebarMenu = function() {
+    this.isOpen = !this.isOpen;
+  };
 }
