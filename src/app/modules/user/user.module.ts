3158933ec35f777a113e320781c895a0504f3d38
Sean Halls <sean@prismwp.com>
2019-08-12 21:29:05 -0700

    Working thru non compile stuff

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserProfileRoutingModule } from '@app/modules/user/user-profile/user-profile-routing.module';
-import { UserAddComponent } from '@app/modules/user/user-add/user-add.component';
-import { UserEditComponent } from '@app/modules/user/user-edit/user-edit.component';
 import { UserDetailComponent } from '@app/modules/user/user-detail/user-detail.component';
 
 @NgModule({
-  declarations: [UserAddComponent, UserEditComponent, UserDetailComponent],
+  declarations: [UserDetailComponent],
   imports: [CommonModule, UserProfileRoutingModule]
 })
 export class UserModule {}
