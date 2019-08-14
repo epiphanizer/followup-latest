97ec49945f5f8df58e1d00f78a20839c4b5144c6
Sean Halls <sean@prismwp.com>
2019-08-14 11:23:02 -0700

    Going thru mockup of corkboard for demo stuff

--- src/app/shell/user-cork-board.module.ts
+++ src/app/shell/user-cork-board.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
-import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { UserCorkBoardComponent } from './user-cork-board.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, IonicModule],
   exports: [UserCorkBoardComponent],
   declarations: [UserCorkBoardComponent],
   providers: []
