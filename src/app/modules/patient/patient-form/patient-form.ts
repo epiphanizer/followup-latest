248802efefe9c2930a83df2def4f6e5f27f5d28f
Sean Halls <sean@prismwp.com>
2019-08-28 17:15:59 -0700

    Getting styles dialed in, working on model for put

--- src/app/modules/patient/patient-form/patient-form.ts
+++ src/app/modules/patient/patient-form/patient-form.ts
@@ -10,5 +10,13 @@ export interface PatientPutBody {
   patientFirstName: string;
   patientMiddleName: string;
   patientLastName: string;
+  patientDiagnosis: string;
+  patientNeedToKnow: string;
+  patientPainScaleAtIntake: number;
+  patientMentalScaleAtIntake: number;
+  patientUrgencyScale: number;
+  patientAdmitDate: Date;
+  patientDischargeDate: Date;
   patientDob: Date;
+  patientActive: number;
 }
