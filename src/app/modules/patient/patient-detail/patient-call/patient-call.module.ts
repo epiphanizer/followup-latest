10fe55ab2bbbac156117f9bc27ec4dcaebc15075
Sean Halls <sean@prismwp.com>
2019-09-09 18:41:40 -0700

    Work on patient call notes highlighting, etc..

--- src/app/modules/patient/patient-detail/patient-call/patient-call.module.ts
+++ src/app/modules/patient/patient-detail/patient-call/patient-call.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PatientNextCallSchedulerComponent } from './patient-next-call-scheduler/patient-next-call-scheduler.component';
-import { PatientNextCallQuestionsComponent } from './patient-next-call-questions/patient-next-call-questions.component';
 
 @NgModule({
-  declarations: [PatientNextCallSchedulerComponent, PatientNextCallQuestionsComponent],
   imports: [CommonModule]
 })
 export class PatientCallModule {}
