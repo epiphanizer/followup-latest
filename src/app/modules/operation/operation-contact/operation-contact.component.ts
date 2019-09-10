3de14d94736044cb7ef736b2f9aa01303932d88e
Sean Halls <sean@prismwp.com>
2019-09-09 20:56:09 -0700

    operation contact component.

---
+++ src/app/modules/operation/operation-contact/operation-contact.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operation-contact',
+  templateUrl: './operation-contact.component.html',
+  styleUrls: ['./operation-contact.component.scss']
+})
+export class OperationContactComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
