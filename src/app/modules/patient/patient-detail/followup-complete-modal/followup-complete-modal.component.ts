0edd95a9cfcd08d507d9b793f34bf322985a8502
Sean Halls <sean@prismwp.com>
2019-09-11 06:15:36 -0700

    going thru patient summary widget, complete modal stuff, ui touchups

--- src/app/modules/patient/patient-detail/followup-complete-modal/followup-complete-modal.component.ts
+++ src/app/modules/patient/patient-detail/followup-complete-modal/followup-complete-modal.component.ts
@@ -3,6 +3,7 @@ import { PatientService } from '../../patient.service';
 import { Patient } from '../../patient';
 import { PatientStatusService, PatientStatus } from '../../patient-status.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-followup-complete-modal',
@@ -13,6 +14,7 @@ export class FollowupCompleteModalComponent implements OnInit {
   patientStatuses: PatientStatus[];
   constructor(
     private fb: FormBuilder,
+    private modalCtrl: ModalController,
     private patientService: PatientService,
     private patientStatusService: PatientStatusService
   ) {}
@@ -30,8 +32,17 @@ export class FollowupCompleteModalComponent implements OnInit {
   archivePatient() {
     alert('Archiving patient');
     var formSubmission = this.followupCompleteForm.getRawValue();
-    var patientStatusLabelId = 1;
-    debugger;
+    var patientStatusLabelId = 5;
+    // debugger;
     this.patientStatusService.addNewPatientStatusByPatientId(this.patient.patientId, patientStatusLabelId);
+    this.dismiss();
+  }
+
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalCtrl.dismiss({
+      dismissed: true
+    });
   }
 }
