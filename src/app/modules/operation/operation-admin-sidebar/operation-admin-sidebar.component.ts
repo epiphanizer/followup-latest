77160a739bcc435d5f35a58e20b0ab7849268b35
Sean Halls <sean@prismwp.com>
2019-09-18 13:30:36 -0700

    operation admin sidebar stuff, more work on ops form

--- src/app/modules/operation/operation-admin-sidebar/operation-admin-sidebar.component.ts
+++ src/app/modules/operation/operation-admin-sidebar/operation-admin-sidebar.component.ts
@@ -1,16 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { formatDate } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+  // ...
+} from '@angular/animations';
+import { Operation } from '../operation.service';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '@app/user';
 
 @Component({
   selector: 'app-operation-admin-sidebar',
   templateUrl: './operation-admin-sidebar.component.html',
-  styleUrls: ['./operation-admin-sidebar.component.scss']
+  styleUrls: ['./operation-admin-sidebar.component.scss'],
+  animations: [
+    trigger('expandSidebar', [
+      state(
+        'open',
+        style({
+          opacity: 1
+        })
+      ),
+      state(
+        'closed',
+        style({
+          opacity: 0
+        })
+      ),
+      transition('open => closed', [animate('0.5s')]),
+      transition('closed => open', [animate('0.25s')])
+    ]),
+    trigger('turnArrow', [
+      state(
+        'open',
+        style({
+          transform: 'rotate(0deg)'
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'rotate(-90deg)'
+        })
+      ),
+      transition('open => closed', [animate('0.125s')]),
+      transition('closed => open', [animate('0.125s')])
+    ])
+  ]
 })
 export class OperationAdminSidebarComponent implements OnInit {
+  @Output() operationChangeEvent = new EventEmitter<number>();
+  selected: {
+    operation: Operation | null;
+  } = {
+    operation: null
+  };
+  isOpen = true;
+  constructor(private route: ActivatedRoute) {}
+  operations: Operation[];
+  user: User;
   todaysDateDay: number;
-  constructor() {}
-
   ngOnInit() {
     this.todaysDateDay = parseInt(formatDate(new Date(), 'dd', 'en'));
   }
+  setActiveOperation = function(operation: Operation) {
+    this.selected.operation = operation;
+    this.operationChangeEvent.emit(operation);
+  };
+  public toggleOperationSidebarMenu = function() {
+    this.isOpen = !this.isOpen;
+  };
 }
