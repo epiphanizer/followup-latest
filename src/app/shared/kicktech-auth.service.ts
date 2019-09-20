f898129346e59e8eb941129671f1e1cd78f20053
Sean Halls <sean@prismwp.com>
2019-09-19 21:06:52 -0700

    starting to scaffold out kt auth service

--- src/app/shared/kicktech-auth.service.ts
+++ src/app/shared/kicktech-auth.service.ts
@@ -1,8 +1,40 @@
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+export interface AuthenticationBodyPost {
+  userName: string;
+  userPassword: string;
+}
 export class KicktechAuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  doLogin(userName: string, userPassword: string) {
+    // do some encryption on what we post over within the authntication body post
+    return this.http.post<AuthenticationBodyPost>('http://followup.care/login', {}).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(e => this.handleAsyncError(e)) // then handle the error
+    );
+  }
+
+  private handleAsyncError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError({
+      message:
+        'We had trouble within the Auth route. \
+          Please contact your IT department and relay this message.'
+    });
+  }
 }
