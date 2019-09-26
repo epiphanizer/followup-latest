d884a7b54940470ca17c15dc748dbc4f2578cabf
Sean Halls <sean@prismwp.com>
2019-09-26 15:47:20 -0700

    Starting to chunk out the patient avatar service calls

--- src/app/modules/patient/patient-avatar/patient-avatar.component.ts
+++ src/app/modules/patient/patient-avatar/patient-avatar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { PatientAvatarServiceService } from './patient-avatar.service';
+import { PatientAvatarService } from './patient-avatar.service';
+import { Patient } from '../patient';
 
 @Component({
   selector: 'app-patient-avatar',
@@ -8,17 +9,21 @@ import { PatientAvatarServiceService } from './patient-avatar.service';
   styleUrls: ['./patient-avatar.component.scss']
 })
 export class PatientAvatarComponent implements OnInit {
+  @Input() patient: Patient;
+  /**
+   * This guy is plaintext encoded base64
+   */
+  avatar: string;
   name = 'Test display image';
   thumbnail: any;
   constructor(private patientAvatarService: PatientAvatarService, private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
-    this.config.getData().subscribe((baseImage: any) => {
+    this.patientAvatarService.getPatientAvatarByPatientId(this.patient.patientId).subscribe((baseImage: any) => {
       //alert(JSON.stringify(data.image));
       let objectURL = 'data:image/jpeg;base64,' + baseImage.image;
 
       this.thumbnail = this.sanitizer.bypassSecurityTrustUrl(objectURL);
     });
-    //console.log(this.setting.snippet)
   }
 }
--- src/app/modules/patient/patient-avatar/patient-avatar.service.ts
+++ src/app/modules/patient/patient-avatar/patient-avatar.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { HttpErrorResponse, HttpClient } from '@angular/common/http';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientAvatarService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+  getPatientAvatarByPatientId(patientId: string): Observable<any> {
+    return this.http.get<any>('/patient/' + patientId + '/avatar').pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(e => this.handleAsyncError(e)) // then handle the error
+    );
+  }
+
+  uploadPatientAvatarByUserId(userId: string, blobData: Blob) {
+    return this.http.post('/user/' + userId + '/avatar', {}).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(e => this.handleAsyncError(e)) // then handle the error
+    );
+  }
+
+  private handleAsyncError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(error);
+  }
 }
