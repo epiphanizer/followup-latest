979e1ec3ca314a09d7f27c06041e08dd83d16aca
Sean Halls <seanhalls@gmail.com>
2019-10-14 01:38:35 -0600

    patching notification email stuff

--- src/app/modules/notification/notification-listing/notification-listing-sidebar/notification-listing-sidebar.component.ts
+++ src/app/modules/notification/notification-listing/notification-listing-sidebar/notification-listing-sidebar.component.ts
@@ -67,7 +67,11 @@ export class NotificationListingSidebarComponent implements OnInit {
   ngOnInit() {
     this.user = this.route.snapshot.data.user;
     this.operations = this.user.operations;
-    this.selected.operation = this.user.operations[0];
+    this.user.operations$.subscribe((data: Operation[]) => {
+      /** Init to the first assigned operation alphabetically */
+      this.selected.operation = data[0];
+      this.operations = data;
+    });
     this.todaysDateDay = parseInt(formatDate(new Date(), 'dd', 'en'));
   }
   setActiveOperation = function(operation: Operation) {
