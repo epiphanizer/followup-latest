b050898a68301bca83c1b8a67fbb9eab0cab2a83
Sean Halls <seanhalls@gmail.com>
2019-10-21 11:14:40 -0400

    corrections

--- src/app/modules/patient/patient-detail/patient-call/patient-call-questions/patient-call-questions.component.ts
+++ src/app/modules/patient/patient-detail/patient-call/patient-call-questions/patient-call-questions.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { PatientCall } from '@app/modules/patient/patient-detail/patient-call/patient-call.service';
 import { Observable } from 'rxjs';
 import { PatientCallQuestionsService, PatientCallQuestion } from './patient-call-questions.service';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-patient-call-questions',
@@ -17,14 +18,21 @@ export class PatientCallQuestionsComponent implements OnInit {
   constructor(private fb: FormBuilder, private patientCallQuestionsService: PatientCallQuestionsService) {}
 
   ngOnInit() {
-    this.questions$ = this.patientCallQuestionsService.getPatientCallQuestionsByPatientCallId(
-      this.patientCall.patientCallId
-    );
+    this.questions$ = this.patientCallQuestionsService
+      .getPatientCallQuestionsByPatientCallId(this.patientCall.patientCallId)
+      .pipe(
+        map((patientCallQuestions: PatientCallQuestion[]) => {
+          patientCallQuestions.forEach(element => {
+            let formArray = this.patientCallQuestionsAnswersForm.controls.patientCallQuestionsAnswers as FormArray;
+            formArray.push(this.fb.control(''));
+          });
+        })
+      );
     this.createForm();
   }
   createForm() {
     this.patientCallQuestionsAnswersForm = this.fb.group({
-      patientCallQuestionsAnswers: this.fb.group({})
+      patientCallQuestionsAnswers: this.fb.array([])
     });
   }
 }
