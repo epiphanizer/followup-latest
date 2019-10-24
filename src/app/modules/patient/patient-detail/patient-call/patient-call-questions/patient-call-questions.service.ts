cc51f6d1922fbb42a924cf5eeb88670af6ee9d15
Sean Halls <seanhalls@gmail.com>
2019-10-24 08:37:53 -0700

    filling in some type details

--- src/app/modules/patient/patient-detail/patient-call/patient-call-questions/patient-call-questions.service.ts
+++ src/app/modules/patient/patient-detail/patient-call/patient-call-questions/patient-call-questions.service.ts
@@ -12,6 +12,7 @@ export interface PatientCallQuestion {
   patientCallQuestionId?: number;
   patientCallQuestion: string;
   patientCallQuestionType: string;
+  patientCallQuestionOrder: number;
   patientCallQuestionIsHighlighted?: boolean;
   patientCallQuestionAnswer?: PatientCallQuestionAnswer;
   patientCallQuestionAnswer$?: Observable<PatientCallQuestionAnswer>;
