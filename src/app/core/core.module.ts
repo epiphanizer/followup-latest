02dc93ed2a5add0cae4f877e3d191f411fb373d3
Sean Halls <seanhalls@gmail.com>
2019-10-25 16:12:29 -0700

    testing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -12,7 +12,6 @@ import { HttpCacheService } from './http/http-cache.service';
 import { ApiPrefixInterceptor } from './http/api-prefix.interceptor';
 import { ErrorHandlerInterceptor } from './http/error-handler.interceptor';
 import { CacheInterceptor } from './http/cache.interceptor';
-import { OperationService } from '@app/modules/operation/operation.service';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule, TranslateModule, RouterModule, AlertsModule],
@@ -31,8 +30,7 @@ import { OperationService } from '@app/modules/operation/operation.service';
     {
       provide: RouteReuseStrategy,
       useClass: RouteReusableStrategy
-    },
-    OperationService
+    }
   ]
 })
 export class CoreModule {
