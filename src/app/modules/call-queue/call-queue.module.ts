aa1d6add7b01b1c6f34ab47f846553ba91172d68
Sean Halls <seanhalls@gmail.com>
2019-10-28 02:44:25 -0700

    correction

--- src/app/modules/call-queue/call-queue.module.ts
+++ src/app/modules/call-queue/call-queue.module.ts
@@ -6,14 +6,13 @@ import { CallQueueRoutingModule } from './call-queue-routing.module';
 import { CallQueueComponent } from './call-queue.component';
 import { CallQueueSidebarComponent } from './call-queue-sidebar/call-queue-sidebar.component';
 import { CallQueueLegendComponent } from './call-queue-legend/call-queue-legend.component';
-import { MonthCalendarModule } from 'simple-angular-calendar';
 import { CallQueueCallHistoryCalendarComponent } from './call-queue-call-history-calendar/call-queue-call-history-calendar/call-queue-call-history-calendar.component';
 import { CallQueuePatientFilterComponent } from './call-queue-patient-filter/call-queue-patient-filter/call-queue-patient-filter.component';
 import { CallQueuePatientListingComponent } from './call-queue-patient-listing/call-queue-patient-listing.component';
 import { PatientModule } from '../patient/patient.module';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, PatientModule, RouterModule, CallQueueRoutingModule, MonthCalendarModule],
+  imports: [CommonModule, IonicModule, PatientModule, RouterModule, CallQueueRoutingModule],
 
   exports: [CallQueueSidebarComponent],
   declarations: [
