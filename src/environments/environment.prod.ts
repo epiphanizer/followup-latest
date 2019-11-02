aa882b460d69e42ff41e8eccbee7764a4fc29f18
Sean Halls <seanhalls@gmail.com>
2019-11-02 02:48:04 -0700

    more descript naming, etc.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  apiUrl: 'https://followup-api.azurewebsites.net:8080',
+  apiUrl: 'http://localhost:8080',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US']
 };
