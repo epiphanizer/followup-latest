43d7619d08d2f2fdd2de948a11910da951fac43e
Sean Halls <seanhalls@gmail.com>
2019-11-02 07:06:12 -0700

    more ui work

--- src/app/modules/patient/patient-detail/patient-detail.component.ts
+++ src/app/modules/patient/patient-detail/patient-detail.component.ts
@@ -106,7 +106,7 @@ export class PatientDetailComponent implements OnInit {
   }
   patientNextCallDateSelectedEventHandler($event: string) {
     let selectedDate = $event;
-    let newDate = formatDate(selectedDate, 'MM-dd-yyyy', 'en-US');
+    let newDate = formatDate(selectedDate, 'MM-dd-yyyy');
     this.patientNextCall.date = newDate;
   }
   patientCallNotesChangeHandler($event: PatientCallNotes) {
