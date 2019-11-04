44b4da7fcba1714108dc6138dd5a727f3246849b
Sean Halls <seanhalls@gmail.com>
2019-11-04 20:54:46 +0530

    Updating for our new build setup, etc.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://followup-api.azurewebsites.net',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US']
 };
