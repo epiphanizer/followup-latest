a373842d303c9e3dd9e594e59073edabdde14856
Sean Halls <seanhalls@gmail.com>
2019-11-04 22:22:14 +0530

    fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,9 +7,9 @@
 import { env } from './.env';
 
 export const environment = {
-  production: true,
+  production: false,
   version: env.npm_package_version,
-  apiUrl: 'https://followup-api.azurewebsites.net',
+  apiUrl: 'https://followup-api.azurewebsites.net/',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US']
 };
