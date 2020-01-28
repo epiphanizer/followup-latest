82341a31eabd5f0bc8ef6d9fdaa60ec880544801
Sean Halls <sean@prismwp.com>
2020-01-28 23:10:20 +0530

    One more try with this TZ

--- src/app/modules/patient/patient-detail/patient-detail.component.ts
+++ src/app/modules/patient/patient-detail/patient-detail.component.ts
@@ -221,7 +221,7 @@ export class PatientDetailComponent implements OnInit {
          */
 
         var dateArray = this.patientNextCall.date.split('-');
-        var isoString = dateArray[2] + '-' + dateArray[0] + '-' + dateArray[1] + 'T20:00:00.000Z';
+        var isoString = dateArray[2] + '-' + dateArray[0] + '-' + dateArray[1] + 'T12:00:00.000Z';
         /**
          * Passing E2E as of now
          */
