c8a35b255f4844381db93d4f7ee4a5d26e546977
Sean Halls <sean@prismwp.com>
2020-03-13 01:13:19 +0530

    Testing compression upload for avatar

--- src/app/modules/patient/patient-avatar-upload/patient-avatar-upload.component.ts
+++ src/app/modules/patient/patient-avatar-upload/patient-avatar-upload.component.ts
@@ -40,25 +40,13 @@ export class PatientAvatarUploadComponent implements OnInit {
 
   dataURItoBlob(dataURI: string) {
     // convert base64 to raw binary data held in a string
-    var byteString = atob(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    var mimeString = dataURI
-      .split(',')[0]
-      .split(':')[1]
-      .split(';')[0];
-
-    // write the bytes of the string to an ArrayBuffer
-    var arrayBuffer = new ArrayBuffer(byteString.length);
-    var _ia = new Uint8Array(arrayBuffer);
-    for (var i = 0; i < byteString.length; i++) {
-      _ia[i] = byteString.charCodeAt(i);
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
     }
-
-    var dataView = new DataView(arrayBuffer);
-    var blob = new Blob([dataView], {
-      type: mimeString
-    });
+    const blob = new Blob([int8Array], { type: 'image/jpeg' });
     return blob;
   }
 
