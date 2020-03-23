b0d33b0acce545ee8d2bb7883255604a1a13ebcf
Sean Halls <sean@prismwp.com>
2020-03-23 05:42:04 +0530

    building

--- src/app/modules/user/user-resolver.service.ts
+++ src/app/modules/user/user-resolver.service.ts
@@ -5,7 +5,8 @@ import { AuthenticationService, HttpService } from '@app/core';
 import { OperationService } from '../operation/operation.service';
 import { map, share, catchError } from 'rxjs/operators';
 import { User } from '@app/modules/user/user';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class UserResolver implements Resolve<User> {
@@ -55,5 +56,20 @@ export class UserResolver implements Resolve<User> {
       share(),
       catchError(error => this.handleAsyncError(error))
     );
+  }
+
+  private handleAsyncError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError({
+      message: 'We had trouble within the authentication service.'
+    });
   }
 }
