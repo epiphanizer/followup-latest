da0f089cdc59c5807f6ba9704787d886cef68afc
Sean Halls <sean@prismwp.com>
2020-06-12 20:50:15 -0600

    More theme stuff, patient form, etc..

--- src/app/modules/patient/patient-form/patient-form.component.ts
+++ src/app/modules/patient/patient-form/patient-form.component.ts
@@ -53,6 +53,7 @@ export class PatientFormComponent implements OnInit {
   patientIntakeQuestionAnswers: PatientIntakeQuestionAnswer[] = [];
   patientIntakeQuestionAnswersToAdd: PatientIntakeQuestionAnswer[] = [];
   patientMaxAdmitDate: string = new Date().getFullYear().toString();
+  // default to 2019 as our first year
   patientMinDischargeDate: string = (new Date().getFullYear() + 1).toString();
   patientMedicalConditions?: string;
   operations: Operation[];
@@ -384,7 +385,7 @@ export class PatientFormComponent implements OnInit {
       });
     });
     console.log(this.patientContactsToRemove);
-
+    debugger;
     /**
      * Get a diff from our original patient contacts
      */
@@ -393,7 +394,7 @@ export class PatientFormComponent implements OnInit {
     });
 
     console.log(this.patientContactsToAdd);
-
+    debugger;
     // Passing E2E
     this.patientContactsToAdd.forEach((patientContact: PatientContact, index: number) => {
       var indexToGrab = parseInt(patientContact.patientContactOrder) - 1;
@@ -428,14 +429,14 @@ export class PatientFormComponent implements OnInit {
         this.editPatient(formSubmission);
       }
       console.log(this.patientContactsToEdit.length);
-
+      debugger;
       this.patientContactsToEdit.forEach((patientContact: PatientContact, index: number) => {
         // Now that we have these, we need to reassign to the form-submitted value.
         var indexToGrab = parseInt(patientContact.patientContactOrder) - 1;
         this.patientContacts[indexToGrab] = formSubmission.patient.patientContacts[indexToGrab];
         var patientContactPut = this.patientContactPutFactory(this.patientContacts[indexToGrab]);
         console.log(patientContactPut);
-
+        debugger;
         this.patientContactService
           .editPatientContactByPatientId(this.patientContacts[indexToGrab].patientContactId, patientContactPut)
           .subscribe(() => {
@@ -453,7 +454,7 @@ export class PatientFormComponent implements OnInit {
   editPatient(formSubmission: any) {
     let patientPutBody = this.formSubmissionFactory(formSubmission);
     console.log(patientPutBody);
-
+    debugger;
     this.patientService.editPatientByPatientId(this.patient.patientId, patientPutBody).subscribe(value => {
       this.toastrService.success('Successfully edited patient!');
       window.location.href = '/operations/' + this.patientForm.get('operation').value + '/patients';
