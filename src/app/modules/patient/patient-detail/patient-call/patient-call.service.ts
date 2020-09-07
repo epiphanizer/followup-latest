9adc764b5944709486f5cfee23da78b878b62e4e
Sean Halls <sean@prismwp.com>
2020-09-07 16:51:16 -0600

    Work preventing duplication

--- src/app/modules/patient/patient-detail/patient-call/patient-call.service.ts
+++ src/app/modules/patient/patient-detail/patient-call/patient-call.service.ts
@@ -109,13 +109,16 @@ export class PatientCallService {
       );
   }
   private handleAsyncError(error: HttpErrorResponse) {
+    if (error.status == 400) {
+      alert('It looks like this call has already finished!');
+      location.reload();
+    }
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
-      alert('It looks like this call has already finished!');
       console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
     }
     // return an observable with a user-facing error message
