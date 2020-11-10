28dc50e9aab18f216c0cc306fdd9206cc05abe17
Sean Halls <sean@prismwp.com>
2020-11-10 01:46:05 -0700

    Fix to envs.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ import { env } from './.env';
 export const environment = {
   production: false,
   version: env.npm_package_version,
-  apiUrl: 'https://followupcare-api.azurewebsites.net',
+  apiUrl: 'https://followupcare-api.azurewebsites.net/',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
 };
