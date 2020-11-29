1bdc53d30227ff321cdecf1e45c68c1886abb72b
Sean Halls <sean@prismwp.com>
2020-11-28 23:38:37 -0700

    Icon cleanup, working on hover colors, etc.

--- src/app/modules/user/user-profile/user-profile-sidebar/user-profile-sidebar.component.ts
+++ src/app/modules/user/user-profile/user-profile-sidebar/user-profile-sidebar.component.ts
@@ -7,10 +7,14 @@ import { formatDate } from '@angular/common';
   styleUrls: ['./user-profile-sidebar.component.scss']
 })
 export class UserProfileSidebarComponent implements OnInit {
+  isOpen: boolean = true;
   todaysDateDay: number;
   constructor() {}
 
   ngOnInit() {
     this.todaysDateDay = parseInt(formatDate(new Date(), 'dd', 'en'));
+  }
+  toggleUserSidebarMenu() {
+    this.isOpen = !this.isOpen;
   }
 }
