aefa1394d9eea33b2eb3cfce77a3a692fc41f628
Sean Halls <sean@prismwp.com>
2020-12-13 16:01:40 -0700

    Team listing

--- src/app/modules/team/team-listing/team-listing.component.ts
+++ src/app/modules/team/team-listing/team-listing.component.ts
@@ -1,63 +1,42 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Operation } from '@app/modules/operation/operation';
+import { Team } from '@app/modules/team/team';
 import { Observable } from 'rxjs';
-import { TeamService } from '../team.service';
-import { Team } from '../team';
 import { map } from 'rxjs/operators';
 import { User } from '@app/modules/user/user';
 import { ActivatedRoute } from '@angular/router';
-import { OperationService } from '@app/modules/operation/operation.service';
+import { TeamService } from '@app/modules/team/team.service';
 
 @Component({
+  providers: [TeamService],
   selector: 'app-team-listing',
   templateUrl: './team-listing.component.html',
   styleUrls: ['./team-listing.component.scss']
 })
 export class TeamListingComponent implements OnInit {
-  @Input() operation: Operation;
+  @Input() team: Team;
   filterBy: string;
   public teams: Team[];
   public teams$: Observable<[Team]> | void = null;
   public selected:
     | {
         filterDate: string;
-        operation: Operation;
-        operation$: Observable<Operation>;
+        operation: Team;
+        operation$: Observable<Team>;
       }
     | any = {};
   selectedSortFlag: string = 'asc';
   user: User;
-  constructor(
-    private operationService: OperationService,
-    private teamService: TeamService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private teamService: TeamService, private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    if (!this.route.snapshot.data.operation) {
-      this.user = this.route.snapshot.data.user;
-
-      if (this.user.userLevel != 1) {
-        this.user.operations$ = this.operationService.getOperationsByUserId(this.user.userId);
-      } else {
-        this.user.operations$ = this.operationService.getAllOperations();
-      }
-      this.user.operations$.subscribe((operations: Operation[]) => {
-        /** Init to the first assigned operation alphabetically */
-        this.selected.operation = operations[0];
-      });
-    } else {
-      this.selected.operation = this.route.snapshot.data.operation;
-    }
-  }
+  ngOnInit() {}
 
   handleDateFilterChangeEvent($event: string) {
     this.selected.filterDate = $event;
   }
-  teamChangeEventHandler($event: Operation) {
-    this.selected.operation = $event;
+  teamChangeEventHandler($event: Team) {
+    this.selected.team = $event;
     this.teams = [];
-    this.teams$ = this.teamService.getTeamMembersByTeamId(this.selected.operation.operationId).pipe(
+    this.teams$ = this.teamService.getTeamMembersByTeamId(this.selected.team.teamId).pipe(
       map((teams: [Team]) => {
         this.teams = teams;
         return teams;
