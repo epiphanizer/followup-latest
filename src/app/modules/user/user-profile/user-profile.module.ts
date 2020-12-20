4f750d82788e395ca51b045d6f2331aac72678bb
Sean Halls <sean@prismwp.com>
2020-12-20 15:33:28 -0700

    Proper import in user profile mod.

--- src/app/modules/user/user-profile/user-profile.module.ts
+++ src/app/modules/user/user-profile/user-profile.module.ts
@@ -5,10 +5,11 @@ import { UserProfileComponent } from './user-profile.component';
 import { IonicModule } from '@ionic/angular';
 import { UserProfileSidebarComponent } from './user-profile-sidebar/user-profile-sidebar.component';
 import { UserModule } from '../user.module';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [UserProfileComponent, UserProfileSidebarComponent],
-  imports: [UserModule, IonicModule, CommonModule, ReactiveFormsModule],
+  imports: [UserModule, IonicModule, CommonModule, NgxMaskModule.forRoot(), ReactiveFormsModule],
   entryComponents: [UserProfileSidebarComponent],
   exports: [UserProfileComponent, UserProfileSidebarComponent]
 })
