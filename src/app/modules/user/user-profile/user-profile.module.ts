484f1fe463a4af3cea45de2c57e10fac7e2f23a7
Sean Halls <sean@prismwp.com>
2020-12-30 21:59:13 -0700

    Killin old sidebar component for profile since it is shared with team anyhow

--- src/app/modules/user/user-profile/user-profile.module.ts
+++ src/app/modules/user/user-profile/user-profile.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserProfileComponent } from './user-profile.component';
 import { IonicModule } from '@ionic/angular';
-import { UserProfileSidebarComponent } from './user-profile-sidebar/user-profile-sidebar.component';
 import { UserModule } from '../user.module';
 import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
-  declarations: [UserProfileComponent, UserProfileSidebarComponent],
+  declarations: [UserProfileComponent],
   imports: [UserModule, IonicModule, CommonModule, NgxMaskModule.forRoot(), ReactiveFormsModule],
-  entryComponents: [UserProfileSidebarComponent],
-  exports: [UserProfileComponent, UserProfileSidebarComponent]
+  entryComponents: [],
+  exports: [UserProfileComponent]
 })
 export class UserProfileModule {}
