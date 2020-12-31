b1ef19c8e55db84fbaa2a5d52fa18f0808e909a0
Sean Halls <sean@prismwp.com>
2020-12-30 21:58:29 -0700

    Porting over sidebar to user profile

--- src/app/modules/user/user-profile/user-profile-sidebar/user-profile-sidebar.component.ts
+++ src/app/modules/user/user-profile/user-profile-sidebar/user-profile-sidebar.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { formatDate } from '@angular/common';
 import { UserRoles } from '../../user';
+import { TeamMember, Team } from '@app/modules/team/team';
+import { LogService } from '@app/shared/log/log.service';
+import { TeamService } from '@app/modules/team/team.service';
 
 @Component({
   selector: 'app-user-profile-sidebar',
@@ -8,15 +11,43 @@ import { UserRoles } from '../../user';
   styleUrls: ['./user-profile-sidebar.component.scss']
 })
 export class UserProfileSidebarComponent implements OnInit {
+  primaryTeam: number = 1;
   isOpen: boolean = true;
+  careRepTeamMembers: TeamMember[] = [];
+  managerTeamMembers: TeamMember[] = [];
+  spanishSpeakingTeamMembers: TeamMember[] = [];
+  teamMembers: TeamMember[];
   todaysDateDay: number;
-  userRoleTypes: UserRoles[];
-  constructor() {}
+  @Input() team: Team;
+  constructor(private logService: LogService, private teamService: TeamService) {}
 
   ngOnInit() {
     this.todaysDateDay = parseInt(formatDate(new Date(), 'dd', 'en'));
-  }
-  toggleUserSidebarMenu() {
-    this.isOpen = !this.isOpen;
+    /**
+     * Until we add in more functionality to add teams, default to primaryTeam
+     */
+
+    this.teamService.getTeamMembersByTeamId(this.primaryTeam).subscribe((teamMembers: TeamMember[]) => {
+      console.log(teamMembers);
+      try {
+        this.teamMembers = teamMembers;
+        this.teamMembers.forEach((teamMember: TeamMember, index: number) => {
+          if (teamMember.teamMemberRoleLabel == 'Manager') {
+            this.managerTeamMembers.push(teamMember);
+          }
+          if (teamMember.teamMemberRoleLabel == 'Care Rep') {
+            this.careRepTeamMembers.push(teamMember);
+          }
+          /**
+           * Check for Spanish speaking
+           */
+          if (teamMember.spanishSpeaking) {
+            this.spanishSpeakingTeamMembers.push(teamMember);
+          }
+        });
+      } catch (error) {
+        this.logService.log(error);
+      }
+    });
   }
 }
