19288a58b4c7635e608fccf16cf311e4b2b1a8ce
Sean Halls <sean@prismwp.com>
2021-03-29 05:03:29 +0530

    A bunch of work on operation form styling, alignment.

--- src/app/modules/operation/operation-form/operation-form.component.ts
+++ src/app/modules/operation/operation-form/operation-form.component.ts
@@ -258,6 +258,7 @@ export class OperationFormComponent implements OnInit {
     this.operation = operation[0];
     var operationFormControls = this.operationForm.get('operation') as FormGroup;
     console.log(operationFormControls);
+    console.log(this.operation);
     operationFormControls.controls.operationId.setValue(this.operation.operationId);
     operationFormControls.controls.operationName.setValue(this.operation.operationName);
     operationFormControls.controls.operationGroupId.setValue(this.operation.operationGroupId);
@@ -268,7 +269,7 @@ export class OperationFormComponent implements OnInit {
     operationFormControls.controls.operationCountryCode.setValue(this.operation.operationCountryCode);
     operationFormControls.controls.operationAreaCode.setValue(this.operation.operationAreaCode);
     operationFormControls.controls.operationPhoneNumber.setValue(this.operation.operationPhoneNumber);
-    operationFormControls.controls.operationActive.setValue(this.operation.operationActive);
+    operationFormControls.controls.operationActive.setValue(this.operation.operationActive ? 1 : 0);
     console.log(operationFormControls);
   }
 
