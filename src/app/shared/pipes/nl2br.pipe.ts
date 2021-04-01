576cb3bcc2cdb642f04ab7d5cd231ed4c56c2d43
Sean Halls <sean@prismwp.com>
2021-04-01 19:01:25 +0530

    Button swapouts on user side.

--- src/app/shared/pipes/nl2br.pipe.ts
+++ src/app/shared/pipes/nl2br.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class nl2brPipe implements PipeTransform {
   transform(value: string): string {
-    value.replace(/%0A/g, '<br/>').replace(/%20/g, '&nbsp;');
+    value.replace(/%0A/g, '<br/><br/>').replace(/%20/g, '&nbsp;');
     return value;
   }
 }
