a2c62aac917b955e26219ea83c08ed8ed5a50c36
Sean Halls <sean@prismwp.com>
2021-04-26 01:25:41 +0530

    Capitalizing first letters where it is appropriate to do so.

--- src/app/shared/directives/capitalize-first.directive.ts
+++ src/app/shared/directives/capitalize-first.directive.ts
@@ -1,8 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[inputCapitalizeFirst]'
 })
 export class CapitalizeFirstDirective {
-  constructor() {}
+  constructor(private ref: ElementRef) {}
+  @HostListener('input', ['$event'])
+  onInput(event: any): void {
+    if (event.target.value.length === 1) {
+      const inputValue = event.target.value;
+      this.ref.nativeElement.value = inputValue.charAt(0).toUpperCase() + inputValue.substring(1);
+    }
+  }
 }
