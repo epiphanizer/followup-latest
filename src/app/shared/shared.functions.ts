e4731309bf002fd2e7847c51f18df03580cb9ee6
Sean Halls <sean@prismwp.com>
2021-07-01 18:39:04 +0530

    Spacing fix, home component work

--- src/app/shared/shared.functions.ts
+++ src/app/shared/shared.functions.ts
@@ -1,7 +1,7 @@
 export class SharedFunctions {
   public returnHTML(value: string): string {
     return value
-      .replace(/%0A/g, '<br/>')
+      .replace(/%0A/g, '</p>')
       .replace(/%20/g, ' ')
       .replace(/%22/g, '"')
       .replace(/%25/g, '%')
